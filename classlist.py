8d7d24dd94a03bcbfd6c77e53400b91b87ca70e1
conceptbamboo <helenlin@uvic.ca>
2012-03-24 21:43:41 -0700

    Added the testing code used to test classlist.py. Currently they are commented out

--- classlist.py
+++ classlist.py
@@ -358,3 +358,37 @@ class classlist:
 			else:
 				print "N{0} is determined when attribute value is '{1}' and have class '{2}'".format( node[0], node[1], node[2])
 		return ""
+
+"""
+def main():
+	data = [
+		[ "Sunny", "17", "High", "False", "No" ],
+		[ "Sunny", "20", "High", "True", "No" ],
+		[ "Overcast", "23", "High", "False", "Yes" ],
+		[ "Rainy", "32", "High", "False", "Yes" ],
+		[ "Rainy", "43", "Normal", "False", "Yes" ],
+		[ "Rainy", "68", "Normal", "True", "No" ],
+		[ "Overcast", "17", "Normal", "True", "Yes" ],
+		[ "Sunny", "20", "High", "False", "No" ],
+		[ "Sunny", "23", "Normal", "False", "Yes" ],
+		[ "Rainy", "32", "Normal", "False", "Yes" ],
+		[ "Sunny", "43", "Normal", "True", "Yes" ],
+		[ "Overcast", "68", "High", "True", "Yes" ],
+		[ "Overcast", "17", "Normal", "False", "Yes" ],
+		[ "Rainy", "20", "High", "True", "No" ]
+	]
+	data2 = [
+		[ "23", "Family", "HIGH" ],
+		[ "17", "Sports", "HIGH" ],
+		[ "43", "Sports", "HIGH" ],
+		[ "68", "Family", "LOW" ],
+		[ "32", "Truck", "LOW" ],
+		[ "20", "Family", "HIGH" ]
+	]
+	CL = classlist(data)
+	print CL.leaves
+	print CL.displayTree()
+			
+if __name__ == '__main__':
+	main()
+"""
