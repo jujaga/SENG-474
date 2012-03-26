bfd73c0975a9041e01b665f728f53bfdf61758e2
jujaga <jujaga@gmail.com>
2012-03-26 14:39:49 -0700

    Update sliq.py to parse arff files with % comments

--- sliq.py
+++ sliq.py
@@ -2,7 +2,7 @@
 # SENG 474
 # Jeremy Ho & Helen Lin
 
-import os, sys, string#, argparse
+import os, sys, string
 from classlist import *
 
 def main():
@@ -15,6 +15,8 @@ def main():
 		sys.exit("{}".format(e))
 	rawinput = []
 	for line in f:
+		if line.startswith("%"):
+			continue
 		rawinput.append(line[:-1].lower()) # ditch trailing newline
 	
 	# Remove non-data lines & format
